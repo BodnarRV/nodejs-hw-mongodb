dbfad690134cf116ef5ddcde855d903049303bce
Rostyslav Bodnar <85370164+BodnarRV@users.noreply.github.com>
2025-01-17 14:03:27 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -5,39 +5,39 @@ import { getEnv } from './utils/getEnv.js';
 import { ENV_VARS } from './constants/env.js';
 import { getContactById, getContacts } from './db/services/contacts.js';
 
-const app = express();
-
-app.use(cors());
-
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
-
-app.get('/contacts', async (req, res) => {
-  const contacts = await getContacts();
-  res.json(contacts);
-});
-
-app.get('/contacts/:contactId', async (req, res) => {
-  const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-
-  if (!contact) {
-    return res.status(404).json({
-      status: 404,
-      message: 'Contact not found',
-    });
-  }
-
-  res.json(contact);
-});
-
-const PORT = getEnv(ENV_VARS.PORT, 3000);
 export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getContacts();
+    res.json(contacts);
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({
+        status: 404,
+        message: 'Contact not found',
+      });
+    }
+
+    res.json(contact);
+  });
+
+  const PORT = getEnv(ENV_VARS.PORT, 3000);
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
