79853b5660a3d81d73f2fbac117d77fab72260a2
Rostyslav Bodnar <85370164+BodnarRV@users.noreply.github.com>
2025-02-03 15:55:34 +0200

    hw5

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,31 +13,43 @@ import {
   updateContactsSchema,
 } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
+import { checkRoles } from '../middlewares/checkRoles.js';
+import { ROLES } from '../constants/index.js';
 
 const router = Router();
+router.use(authenticate);
 
-router.get('/contacts', ctrlWrapper(getContactsController));
+router.get(
+  '/contacts',
+  checkRoles(ROLES.TEACHER),
+  ctrlWrapper(getContactsController),
+);
 
 router.get(
   '/contacts/:contactId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
   isValidId,
   ctrlWrapper(getContactByIdController),
 );
 
 router.post(
   '/contacts',
+  checkRoles(ROLES.TEACHER),
   validateBody(createContactsSchema),
   ctrlWrapper(createContactController),
 );
 
 router.delete(
   '/contacts/:contactId',
+  checkRoles(ROLES.TEACHER),
   isValidId,
   ctrlWrapper(deleteContactController),
 );
 
 router.patch(
   '/contacts/:contactId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
   isValidId,
   validateBody(updateContactsSchema),
   ctrlWrapper(patchContactController),
--- src/server.js
+++ src/server.js
@@ -3,14 +3,16 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { getEnv } from './utils/getEnv.js';
 import { ENV_VARS } from './constants/env.js';
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
 
 export const setupServer = () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
 
   app.use(
     pino({
@@ -20,7 +22,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(contactsRouter);
+  app.use(router);
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
